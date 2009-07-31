dec1292328e1f6a6b610664c4f4912643bbb21ea
Benjamin Goldenberg <bgoldenberg@gmail.com>
2009-07-30 21:21:58 -0700

    stdout

--- scripts/add_kernels.py
+++ scripts/add_kernels.py
@@ -112,6 +112,9 @@ def add_kernels(kernel1, kernel2, output_filename, weight1=1, weight2=1):
                           '--alpha2=%.3f' % weight2, 
                           kernel1, kernel2]
     
+    assert os.path.exists(kernel1)
+    assert os.path.exists(kernel2)
+    
     popen = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     stdout, stderr = popen.communicate()
