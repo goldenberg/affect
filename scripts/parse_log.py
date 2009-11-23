fff435e4a250cf2704bead848e0180ba4e9ae1a7
Benjamin Goldenberg <bgoldenberg@gmail.com>
2009-11-23 01:42:07 -0800

    change env to python2.6

--- scripts/parse_log.py
+++ scripts/parse_log.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.6
 # encoding: utf-8
 help_message = """%prog WAV_FILENAME LOG_FILENAME [options]
 This script parses an Apache web server log for requests for the SESDC
